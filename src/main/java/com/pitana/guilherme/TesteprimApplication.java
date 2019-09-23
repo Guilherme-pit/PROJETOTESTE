6082ed99c5b4c70eee48956336dd479218927f4f
Guilherme-pit <git config --global user.email Guilhermepitana@outlook.com>
2019-09-23 11:11:00 -0300

    Projeto criado

---
+++ src/main/java/com/pitana/guilherme/TesteprimApplication.java
@@ -0,0 +1,13 @@
+package com.pitana.guilherme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteprimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteprimApplication.class, args);
+	}
+
+}
